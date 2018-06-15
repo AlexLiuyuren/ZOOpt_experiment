357da147b79d0d753b3bbde471d6f61ec36c1027
Yuren Liu <liuyr@lamda.nju.edu.cn>
2018-06-15 16:04:33 +0800

    update

--- plot/get_ave_std.py
+++ plot/get_ave_std.py
@@ -19,13 +19,17 @@ def get_ave_std(package_name, func_name, append_name):
     filename = package_name + '/log/' + func_name + '/' + func_name + '_' + append_name + '.txt'
     result = get(filename, 10)
     save_name = package_name + '/log/' + func_name + '/ave_std/' + func_name + '_' + append_name + '.txt'
+    with open(save_name, 'w') as f:
+        f.truncate()
     np.savetxt(save_name, result)
 
 
 def get_ave_std_scale(package_name, func_name):
     filename = package_name + '/log/' + func_name + '/' + func_name + '_scale.txt'
-    result = get(filename, 10)
+    result = get(filename, 5)
     save_name = package_name + '/log/' + func_name + '/ave_std/' + func_name + '_scale.txt'
+    with open(save_name, 'w') as f:
+        f.truncate()
     np.savetxt(save_name, result)
 
 
@@ -40,7 +44,7 @@ def ZOOpt_ave_std(func_name):
 
 def CMAES_ave_std(func_name):
     get_ave_std('CMAES_exp', func_name, '20')
-    get_ave_std('CMAES_exp', func_name, '10000')
+    # get_ave_std('CMAES_exp', func_name, '10000')
     get_ave_std('CMAES_exp', func_name, 'noisy')
     get_ave_std('CMAES_exp', func_name, 'noisy_without_nh')
     get_ave_std_scale('CMAES_exp', func_name)
@@ -64,11 +68,11 @@ if __name__ == '__main__':
     # ZOOpt_ave_std('ackley')
     # ZOOpt_ave_std('sphere')
 
-    # CMAES_ave_std('griewank')
-    # CMAES_ave_std('rastrigin')
+    CMAES_ave_std('griewank')
+    CMAES_ave_std('rastrigin')
     CMAES_ave_std('schwefel')
-    # CMAES_ave_std('ackley')
-    # CMAES_ave_std('sphere')
+    CMAES_ave_std('ackley')
+    CMAES_ave_std('sphere')
 
     # DEAP_ave_std('griewank')
     # DEAP_ave_std('rastrigin')
